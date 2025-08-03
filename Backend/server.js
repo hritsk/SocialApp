698d3a95872218b91dc525c13d0b8c343ba38f1e
Hritik Kumar <hritiksingh.23k@gmail.com>
2025-08-04 01:25:24 +0530

    changes

--- Backend/server.js
+++ Backend/server.js
@@ -10,5 +10,6 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
+
 });
